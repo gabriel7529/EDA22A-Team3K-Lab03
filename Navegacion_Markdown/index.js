b27023c9196a57d1857d059e93cbd51887286dbf
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-14 16:10:05 -0500

    Se arreglo en index.js usando un método readdir, para leer los archivos de un directorio, y luego usar el replace para hacer saltos de linea, en el index.html se puso un id, para colocar los nombres de los archivos.

--- Navegacion_Markdown/index.js
+++ Navegacion_Markdown/index.js
@@ -1,29 +1,29 @@
+const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
+
+app.use(cors())
 app.use(express.static('pub'))
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
 });
 
-app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'index.html'))
-})
+app.get('/lista', (request, response) => {
+    fs.readdir(path.resolve(__dirname,'./priv'),'utf8', 
+      (err,data) => {
+	if (err){ 
+	  console.error(err);
+	  response.status(500).json({
+	    error:'menssaje'
+	  })
+	  return
+	}
+	const f = Array.isArray(data)
+	response.json({
+	  text: data.toString().replace(/,/,'<br>')
+	})
+      })
 
-app.get('/recitar', (request, response) => {
-    fs.readFile(path.resolve(__dirname, 'priv/lista.txt'), 'utf8',
-        (err, data) => {
-            if (err) {
-                console.error(err)
-                response.status(500).json({
-                    error: 'message'
-                })
-                return
-            }
-            response.json({
-                text: data.replace(/\n/g, '<br>')
-            })
-        })
-    //
 })
