641dc28944d6b6cb73822ccf24e5800105fd0178
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-15 11:46:49 -0500

    Se elimana los archivos .txt y se crean dos archivos md, se cambia la expresión regular y además se verifica si la data que recibe el fecth es un array.

--- Navegacion_Markdown/index.js
+++ Navegacion_Markdown/index.js
@@ -22,7 +22,7 @@ app.get('/lista', (request, response) => {
 	}
 	const f = Array.isArray(data)
 	response.json({
-	  text: data.toString().replace(/.txt(,)*/g,'<br>')
+	  text: data.toString().replace(/.md(,)*/g,'<br>')
 	})
       })
 
