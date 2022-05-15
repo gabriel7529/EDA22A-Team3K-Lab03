89f926fda05c4b1576ddc8f67ec32fcf0a0b99ca
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-15 14:40:41 -0500

    Se añade el const bp que llama que usa al body-parse a lo cual tambien se agrega un app.get para buscar un archivo de nombre del contenido de la variable name, todavia no funciona, en el index.js y en el index.html, se da una sección para la visualización y tambien se modifica las variables formV y botonV

--- Navegacion_Markdown/index.js
+++ Navegacion_Markdown/index.js
@@ -2,13 +2,20 @@ const cors = require('cors')
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const bp = require('body-parser')
 const app = express()
 
 app.use(cors())
-app.use(express.static('pub'))
+app.use(express.static('cliente'))
 app.listen(3000, () => {
     console.log("Escuchando en: http://localhost:3000")
 });
+
+app.use(express.static('pub'))
+app.use(bp.json())
+app.use(bp.urlencoded({
+    extended: true
+}))
 
 app.get('/lista', (request, response) => {
     fs.readdir(path.resolve(__dirname,'./priv'),'utf8', 
@@ -20,10 +27,28 @@ app.get('/lista', (request, response) => {
 	  })
 	  return
 	}
-	const f = Array.isArray(data)
 	response.json({
 	  text: data.toString().replace(/.md(,)*/g,'<br>')
 	})
       })
 
 })
+app.get('/BuscarTitulos', (req, res) => {
+	let name = req.query.titulo
+	console.log(name)
+	fs.readFile(path.resolve(__dirname, 'priv/'+ name + '.md'), 'utf8',
+        (err, data) => {
+            if (err) {
+                console.error(err)
+                res.status(500).json({
+                    error: 'message'
+                })
+                return
+            }
+            res.json({
+                text: data.replace(/\n/g, '<br>')
+            })
+        })
+})
+
+
